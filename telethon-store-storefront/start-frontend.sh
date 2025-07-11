759862b22d5948a1e2fc0c259cac0924fa3bb971
MustacheTrashStash <matthewbushgames@gmail.com>
2025-07-11 02:18:08 -0400

    Simplify configuration and add packageManager field to explicitly use npm

--- telethon-store-storefront/start-frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Force npm usage and start the frontend
-echo "Starting Next.js frontend with npm..."
-echo "Setting up yarn wrapper..."
-chmod +x ./yarn
-export PATH=".:$PATH"
-npm start
--- telethon-store/start-backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Force npm usage and start the backend
-echo "Starting Medusa backend with npm..."
-echo "Setting up yarn wrapper..."
-chmod +x ./yarn
-export PATH=".:$PATH"
-npm start
