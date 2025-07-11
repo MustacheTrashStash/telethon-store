f18f969bfca80c64eb4110cd3e1e93a0389bde7d
MustacheTrashStash <matthewbushgames@gmail.com>
2025-07-11 02:50:56 -0400

    Simplify: install yarn globally and use npm normally

--- telethon-store/start-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install yarn compatibility layer globally
-chmod +x ./yarn
-export PATH="$(pwd):$PATH"
-echo "PATH updated to include yarn compatibility layer"
-
-# Start the application
-npm start
